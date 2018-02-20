09b6a6e0395aa7da8b22fc34ef4cb411cabfcf33
Glaaki <glaaki@protonmail.com>
2018-02-20 14:21:26 +0100

    add count tries, check win, use last try as default for buttons

--- MasterMind.rb
+++ MasterMind.rb
@@ -6,12 +6,19 @@ COLORS = ["red", "blue", "green", "yellow", "cyan", "magenta"]
 class MasterMind
   attr_accessor :solutionColors
   attr_accessor :userColors
+  attr_accessor :nbrTries
   def initialize
     @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
+    @nbrTries = 0
   end
 
  def reset
    @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
+   @nbrTries = 0
+ end
+
+ def checkWin
+   return @solutionColors == @userColors
  end
 
 end
@@ -19,12 +26,19 @@ end
 game = MasterMind.new
 
 get '/' do
-  game.reset
   erb :index
 end
 
 get '/game' do
-  game.userColors = params[:userColor1]
-  puts game.userColors
-  erb :game, :locals => {:colors => COLORS, :solutionColors => game.solutionColors}
+  if params[:newGame] == "false"
+    game.nbrTries+=1
+    game.userColors = [params[:userColor1],params[:userColor2],params[:userColor3],params[:userColor4]]
+    puts game.checkWin, game.nbrTries
+    erb :game, :locals => {:nbrTries => game.nbrTries, :colors => COLORS, :solutionColors => game.solutionColors, :lastUserInput => game.userColors}
+  else
+  game.reset
+  erb :game, :locals => {:nbrTries => game.nbrTries, :colors => COLORS, :solutionColors => game.solutionColors, :lastUserInput => ["red","red","red","red"]}
+  end
+
+
 end
