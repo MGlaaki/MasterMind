422147564d9cfc7ce124ced48d48bca7ab94a852
Glaaki <glaaki@protonmail.com>
2018-02-20 17:48:53 +0100

    working, but very ugly and solution revealed

--- MasterMind.rb
+++ MasterMind.rb
@@ -7,6 +7,8 @@ class MasterMind
   attr_accessor :solutionColors
   attr_accessor :userColors
   attr_accessor :nbrTries
+  attr_accessor :history
+  attr_accessor :score
   def initialize
     @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
     @nbrTries = 0
@@ -15,10 +17,37 @@ class MasterMind
  def reset
    @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
    @nbrTries = 0
+   @history = []
  end
 
  def checkWin
-   return @solutionColors == @userColors
+   return @solutionColors == @userColors[0..3]
+ end
+
+ def updateHistory(lastParams)
+   @userColors = lastParams
+   paramsScore
+   @history << @userColors
+
+ end
+
+ def paramsScore
+   ok = 0
+   presence = 0
+   @userColors.each_with_index do |color, index|
+     if color == @solutionColors[index]
+       ok+=1
+     end
+   end
+
+   COLORS.each do |color|
+     presence += [@userColors.count(color),@solutionColors.count(color)].min
+   end
+   presence -= ok
+
+   puts "ok #{ok}"
+   puts "presence #{presence}"
+   @userColors << [ok,presence]
  end
 
 end
@@ -32,13 +61,20 @@ end
 get '/game' do
   if params[:newGame] == "false"
     game.nbrTries+=1
-    game.userColors = [params[:userColor1],params[:userColor2],params[:userColor3],params[:userColor4]]
-    puts game.checkWin, game.nbrTries
-    erb :game, :locals => {:nbrTries => game.nbrTries, :colors => COLORS, :solutionColors => game.solutionColors, :lastUserInput => game.userColors}
+    game.updateHistory([params[:userColor1],params[:userColor2],params[:userColor3],params[:userColor4]])
+    if game.checkWin || game.nbrTries>=10
+      redirect '/result'
+    end
+
+
+    erb :game, :locals => {:history => game.history, :nbrTries => game.nbrTries, :colors => COLORS, :solutionColors => game.solutionColors, :lastUserInput => game.userColors}
   else
   game.reset
-  erb :game, :locals => {:nbrTries => game.nbrTries, :colors => COLORS, :solutionColors => game.solutionColors, :lastUserInput => ["red","red","red","red"]}
+  erb :game, :locals => {:history => game.history, :nbrTries => game.nbrTries, :colors => COLORS, :solutionColors => game.solutionColors, :lastUserInput => ["red","red","red","red"]}
   end
 
+end
 
+get '/result' do
+    erb :result, :locals => {:victory => game.checkWin}
 end
