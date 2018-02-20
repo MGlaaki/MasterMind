c147d4084bf90d4c0a6489129991025c77f9a262
Glaaki <glaaki@protonmail.com>
2018-02-20 07:29:01 +0100

    Split views in 2 index/game

--- MasterMind.rb
+++ MasterMind.rb
@@ -1,6 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 
+class MasterMind
+  def initialize
+    puts "bonsoir"
+  end
+end
+
 get '/' do
   erb :index
+end
+
+get '/game' do
+  game = MasterMind.new if params[:newGame] == "true"
+  erb :game
 end
