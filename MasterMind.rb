f0a02651870bd2e41a0503e3634c0d9c63059430
Glaaki <glaaki@protonmail.com>
2018-02-20 11:34:37 +0100

    implemented sinatra gets the user color input

--- MasterMind.rb
+++ MasterMind.rb
@@ -5,17 +5,26 @@ COLORS = ["red", "blue", "green", "yellow", "cyan", "magenta"]
 
 class MasterMind
   attr_accessor :solutionColors
+  attr_accessor :userColors
   def initialize
     @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
   end
+
+ def reset
+   @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
+ end
+
 end
 
+game = MasterMind.new
+
 get '/' do
+  game.reset
   erb :index
 end
 
 get '/game' do
-  game = MasterMind.new if params[:newGame] == "true"
-  puts game.solutionColors[0]
+  game.userColors = params[:userColor1]
+  puts game.userColors
   erb :game, :locals => {:colors => COLORS, :solutionColors => game.solutionColors}
 end
