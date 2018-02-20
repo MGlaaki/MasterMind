4c39b2e59870fa7b2e05d8077163dac13d7c307c
Glaaki <glaaki@protonmail.com>
2018-02-20 10:02:24 +0100

    color selection

--- MasterMind.rb
+++ MasterMind.rb
@@ -1,9 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
 
+COLORS = ["red", "blue", "green", "yellow", "cyan", "magenta"]
+
 class MasterMind
+  attr_accessor :solutionColors
   def initialize
-    puts "bonsoir"
+    @solutionColors = (1..4).to_a.map{|i| COLORS.sample}
   end
 end
 
@@ -13,5 +16,6 @@ end
 
 get '/game' do
   game = MasterMind.new if params[:newGame] == "true"
-  erb :game
+  puts game.solutionColors[0]
+  erb :game, :locals => {:colors => COLORS, :solutionColors => game.solutionColors}
 end
